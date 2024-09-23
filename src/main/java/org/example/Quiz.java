9351cbdb907ad70b545b85671d4cf813ecc4c0a6
scarfinor <scarfinor@gmail.com>
2024-09-23 17:25:26 -0500

    git commit added: Questions.java QuizRunner.java Quiz.java MultipleChoice.java CheckBox.java TrueFalse.java

---
+++ src/main/java/org/example/Quiz.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Quiz {
+   private ArrayList<String> questions;
+
+   public void Quiz() {
+       this.questions = questions;
+   }
+
+    public ArrayList<String> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(ArrayList<String> questions) {
+        this.questions = questions;
+    }
+
+    public void runQuiz() {
+
+    }
+
+    public void gradeQuiz() {
+
+    }
+}
---
+++ src/main/java/org/example/TrueFalse.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public class TrueFalse extends Questions {
+}
