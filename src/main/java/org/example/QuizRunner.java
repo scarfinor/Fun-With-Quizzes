1400205cc1bc38257e2058387ee284d7982111f0
scarfinor <scarfinor@gmail.com>
2024-09-23 18:49:03 -0500

    working with check box!!!!

--- src/main/java/org/example/QuizRunner.java
+++ src/main/java/org/example/QuizRunner.java
@@ -6,18 +6,14 @@ public class QuizRunner {
     public static void main(String[] args) {
         Quiz quiz = new Quiz();
 
-        // Add multiple choice question
         quiz.addQuestion(new MultipleChoice("What is the capital of America?",
                 Arrays.asList("St.Louis", "Washington DC", "Atlanta", "Bend"), "Washington DC"));
 
-        // Add checkbox question
         quiz.addQuestion(new CheckBox("Select all prime numbers:",
                 Arrays.asList("2", "3", "4", "5", "6"), Arrays.asList("2", "3", "5")));
 
-        // Add true/false question
         quiz.addQuestion(new TrueFalse("A square has 5 sides.", false));
 
-        // Run the quiz
         quiz.runQuiz();
     }
 }
